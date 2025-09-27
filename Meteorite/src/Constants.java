240bc43e5086afbd5d5643f61682852bb6eeb4d5
TsumiyaZ <watcharakornnigc@gmail.com>
2025-09-27 20:47:55 +0700

    a

--- Meteorite/src/Constants.java
+++ Meteorite/src/Constants.java
@@ -1,7 +1,16 @@
 public class Constants {
     public static final int WINDOW_HEIGHT = 1000;
     public static final int WINDOW_WIDTH = 1000;
-    public static final String[] imageFiles = {"meteorite2.png", "meteorite3.png", "meteorite4.png", "meteorite5.png", "meteorite6.png"};
+    public static final String[] imageFiles = {
+            "meteorite1.png",
+            "meteorite2.png",
+            "meteorite3.png",
+            "meteorite4.png",
+            "meteorite5.png",
+            "meteorite6.png",
+            "meteorite7.png",
+            "meteorite8.png" 
+        };
     public static final int amount_meteorite = 100;
     public static final int Scale_Meteorite = 50;
 
